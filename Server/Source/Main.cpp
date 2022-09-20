d0fc0be92f8efc8a71bc056c4ba7f46f4a0758f6
Martin VANAUD <martin.vanaud@epitech.eu>
2022-09-20 08:37:44 +0200

    setup(boilerplate) added base files and cmake build for projet

---
+++ Server/Source/Main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPP-500-PAR-5-1-rtype-martin.vanaud
+** File description:
+** Main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return (0);
+}
