6b603d152122d9c8d339f84cfdac70aa32252307
Martin VANAUD <martin.vanaud@epitech.eu>
2022-09-20 10:37:11 +0200

    ci/cd(github actions) added unit tests base

---
+++ Client/Test/Test.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPP-500-PAR-5-1-rtype-martin.vanaud
+** File description:
+** Test
+*/
+
+#include <assert.h>
+#include <criterion/assert.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(Core, coreTest)
+{
+    assert(true);
+}
