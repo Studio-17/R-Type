e1137a9c699537c15cf64a29f360aea8117ba651
Clément FERNANDES <clement.fernandes@epitech.eu>
2022-09-30 12:17:17 +0200

    feat(graphicallib): added the class Text in the GraphicalLib
    the function 'text' can create a new text
    the function 'draw' allows to draw the text
    the function 'setposition' allows to set the position of the text
    the function 'getposition' returns the position of the text
    the function 'settext' allow to set the content of the text
    the function 'gettext' returns the content of the text
    the function 'setfontsize' allow to set the font of the text
    the function 'setcolor' allows to set the color of the text

---
+++ Client/Source/GraphicalLib/Text/Text.hpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPP-500-PAR-5-1-rtype-martin.vanaud
+** File description:
+** Text
+*/
+
+#ifndef TEXT_HPP_
+#define TEXT_HPP_
+
+#include <string>
+#include "Position.hpp"
+
+class Text {
+    public:
+        Text(std::string const &filename, std::string const &text, int fontSize, Color const &color, Position const &position);
+        ~Text();
+
+        void draw();
+
+        void setPosition(Position const &position);
+        Position getPosition();
+
+        void setText(std::string const &text);
+        std::string getText();
+
+        void setFontSize(int fontSize);
+
+        void setColor(Color const &color);
+
+    protected:
+    private:
+        Position _position;
+        Font _font;
+
+        Color _color;
+        int _greenColor;
+        int _redColor;
+        int _blueColor;
+        int _alphaColor;
+
+        std::string _text;
+        int _fontSize = 20;
+};
+
+#endif /* !TEXT_HPP_ */
