f61b05b2cd5939eae75e1a23b4b5b442f81adc94
Martin VANAUD <martin.vanaud@epitech.eu>
2022-10-09 13:57:27 +0200

    unit-tests(framework) switched to Criterion instead of GTest, added CMakelists rules and github workflow

---
+++ Tests/Test.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** Created by casimir,
+*/
+
+#include <criterion/assert.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+#include <Entity.hpp>
+
+Test(EntityTest, Entity__ctor__)
+{
+    Entity entity(1);
+}
