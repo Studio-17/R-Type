46092a4aaf44e73532ef4cf85e9ce2430d40d9b4
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-10-16 17:25:24 +0200

    fix(player movement): fixed movement with negative direction to an enum to handle with unsigned short values

--- Client/Source/System/ControlSystem/ControlSystem.cpp
+++ Client/Source/System/ControlSystem/ControlSystem.cpp
@@ -21,35 +21,28 @@ ControlSystem::ControlSystem()
 void ControlSystem::operator()(Registry &registry, Sparse_array<component::cposition_t> &positions, Sparse_array<component::cvelocity_t> &velocities, Sparse_array<component::ckeyboard_t> &keyboards, Sparse_array<component::cnetwork_queue_t> &network, Sparse_array<component::cserverid_t> &serverId)
 {
     auto &key = keyboards[0];
-    int x = 0;
-    int y = 0;
+    uint16_t x = 0;
+    uint16_t y = 0;
 
     if (key->keyboard->isBeingPressed(key->keyboard->getKeyUpCharCode()))
-        y = -1;
+        y = packet_move::DIRECTION::MOIN;
     if (key->keyboard->isBeingPressed(key->keyboard->getKeyDownCharCode()))
-        y = 1;
+        y = packet_move::DIRECTION::PLUS;
     if (key->keyboard->isBeingPressed(key->keyboard->getKeyLeftCharCode()))
-        x = -1;
+        x = packet_move::DIRECTION::MOIN;
     if (key->keyboard->isBeingPressed(key->keyboard->getKeyRightCharCode()))
-        x = 1;
+        x = packet_move::DIRECTION::PLUS;
     if (key->keyboard->hasBeenPressed(key->keyboard->getKeySpaceCharCode()))
-        x = 2;
+        x = 3;
     if (x || y)
         addToNetworkQueue(x, y, network, serverId[1]->id);
 }
 
-void ControlSystem::addToNetworkQueue(int x, int y, Sparse_array<component::cnetwork_queue_t> &network, std::size_t serverId) {
-    if (x == 2) {
-        packet_shoot packet = {.id = 1};
-        std::vector<byte> tmp = serialize_header::serializeHeader<packet_shoot>(NETWORK_CLIENT_TO_SERVER::PACKET_TYPE::SHOOT, packet);
-        network[FORBIDDEN_IDS::NETWORK].value().toSendNetworkQueue.push(tmp);
-    }
-    else {
-        packet_move packet;
-        packet.x = x;
-        packet.y = y;
-        packet.playerId = 1;
-        std::vector<byte> tmp = serialize_header::serializeHeader<packet_move>(1, packet);
-        network[FORBIDDEN_IDS::NETWORK].value().toSendNetworkQueue.push(tmp);
-    }
+void ControlSystem::addToNetworkQueue(uint16_t x, uint16_t y, Sparse_array<component::cnetwork_queue_t> &network, std::size_t serverId) {
+    std::vector<byte> tmp;
+    if (x == 3)
+        tmp = serialize_header::serializeHeader<packet_shoot>(NETWORK_CLIENT_TO_SERVER::PACKET_TYPE::SHOOT, {1});
+    else
+        tmp = serialize_header::serializeHeader<packet_move>(1, {1, x, y});
+    network[FORBIDDEN_IDS::NETWORK].value().toSendNetworkQueue.push(tmp);
 }
