0be971ef94a14e3e4c5f75ab96dbe2c828e6556a
Martin VANAUD <martin.vanaud@sony.com>
2022-10-16 17:45:25 +0200

    unit-tests(criterion) addded 4 testing cases:
    Component: 6 tests for some of our components
    Network: initialization of network module using asio
    Registry: add, kill and register components
    System: add and run a system

--- Tests/Source/TestRegistry.cpp
+++ Tests/Source/TestRegistry.cpp
@@ -9,8 +9,8 @@
 #include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-#include "Entity.hpp"
 #include "Registry.hpp"
+#include "Entity.hpp"
 #include "SparseArray.hpp"
 
 #include "CDamage.hpp"
@@ -22,33 +22,9 @@
 #include "CType.hpp"
 #include "CVelocity.hpp"
 
-class MoveSystem {
-    public:
-        MoveSystem() {};
-
-        ~MoveSystem() = default;
-
-        void operator()(Registry &registry,
-                        Sparse_array<component::cnetwork_queue_t> &netqueue,
-                        Sparse_array<component::cdirection_t> &direction,
-                        Sparse_array<component::cposition_t> &position,
-                        Sparse_array<component::cvelocity_t> &velocity) {
-            for (unsigned short index = 0; index < position.size(); index++) {
-                position[index]->x += (velocity[index]->velocity * direction[index]->x);
-                if (position[index]->x > 1920)
-                    position[index]->x = 0;
-                position[index]->y += (velocity[index]->velocity * direction[index]->y);
-            }
-        };
-
-    protected:
-    private:
-};
-
-Test(SystemAddTest, SystemTests)
+Test(RegistryRegisterTest, RegistryTests)
 {
     Registry registry;
-    MoveSystem _moveSystem;
 
     registry.register_component<component::cdamage_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
     registry.register_component<component::cdirection_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
@@ -57,14 +33,11 @@ Test(SystemAddTest, SystemTests)
     registry.register_component<component::cposition_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
     registry.register_component<component::cvelocity_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
     registry.register_component<component::cnetwork_queue_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
-
-    registry.add_system(_moveSystem, registry.get_components<component::cnetwork_queue_t>(), registry.get_components<component::cdirection_t>(), registry.get_components<component::cposition_t>(), registry.get_components<component::cvelocity_t>());
 }
 
-Test(SystemRunTest, SystemTests)
+Test(RegistryAddTest, RegistryTests)
 {
     Registry registry;
-    MoveSystem _moveSystem;
 
     registry.register_component<component::cdamage_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
     registry.register_component<component::cdirection_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
@@ -74,7 +47,40 @@ Test(SystemRunTest, SystemTests)
     registry.register_component<component::cvelocity_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
     registry.register_component<component::cnetwork_queue_t>([](Registry &registry, Entity const &entity) -> void {}, [](Registry &registry, Entity const &entity) -> void {});
 
-    registry.add_system(_moveSystem, registry.get_components<component::cnetwork_queue_t>(), registry.get_components<component::cdirection_t>(), registry.get_components<component::cposition_t>(), registry.get_components<component::cvelocity_t>());
+    Entity networkEntity = registry.spawn_entity();
 
-    registry.run_systems();
+    component::cdamage_t damage = { 0 };
+    registry.add_component<component::cdamage_t>(registry.entity_from_index(networkEntity), std::move(damage));
+    if (registry.get_components<component::cdamage_t>()[0].value().damage != 0)
+        cr_assert(true);
+
+    component::cdirection_t direction = { 0, 0 };
+    registry.add_component<component::cdirection_t>(registry.entity_from_index(networkEntity), std::move(direction));
+    if (registry.get_components<component::cdirection_t>()[0].value().x != 0)
+        cr_assert(true);
+
+    component::chealth_t health = {10 };
+    registry.add_component<component::chealth_t>(registry.entity_from_index(networkEntity), std::move(health));
+    if (registry.get_components<component::chealth_t>()[0].value().health != 0)
+        cr_assert(true);
+
+    component::chitbox_t hitbox = {10, 10};
+    registry.add_component<component::chitbox_t>(registry.entity_from_index(networkEntity), std::move(hitbox));
+    if (registry.get_components<component::chitbox_t>()[0].value().height != 0)
+        cr_assert(true);
+
+    component::cposition_t position = {10, 10};
+    registry.add_component<component::cposition_t>(registry.entity_from_index(networkEntity), std::move(position));
+    if (registry.get_components<component::cposition_t>()[0].value().x != 0)
+        cr_assert(true);
+
+    component::cvelocity_t velocity = {10 };
+    registry.add_component<component::cvelocity_t>(registry.entity_from_index(networkEntity), std::move(velocity));
+    if (registry.get_components<component::cvelocity_t>()[0].value().velocity != 0)
+        cr_assert(true);
+
+    component::cnetwork_queue_t network = {};
+    registry.add_component<component::cnetwork_queue_t>(registry.entity_from_index(networkEntity), std::move(network));
+    if (!registry.get_components<component::cnetwork_queue_t>()[0].value().receivedNetworkQueue.empty())
+        cr_assert(true);
 }
