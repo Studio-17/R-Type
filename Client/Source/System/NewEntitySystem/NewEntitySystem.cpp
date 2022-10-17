d5357c66d624693f7db3984d8eb8b53addfcfd47
Victor PALLE <victor.palle@epitech.eu>
2022-10-17 17:31:45 +0200

    feat(client): added timer and handle timer

--- Client/Source/System/NewEntitySystem/NewEntitySystem.cpp
+++ Client/Source/System/NewEntitySystem/NewEntitySystem.cpp
@@ -63,7 +63,7 @@ void NewEntitySystem::addBullet(Registry &registry, packet_new_entity &newEntity
     component::cserverid_t serverId = {.id = newEntity.id};
     registry.add_component<component::cserverid_t>(registry.entity_from_index(bullet), std::move(serverId));
 
-    component::cvelocity_t velocity = {.velocity = 8};
+    component::cvelocity_t velocity = {.velocity = 4};
     registry.add_component<component::cvelocity_t>(registry.entity_from_index(bullet), std::move(velocity));
     registry.add_component<component::ckilled_t>(registry.entity_from_index(bullet), {false});
 }
@@ -81,7 +81,7 @@ void NewEntitySystem::addEnemy(Registry &registry, packet_new_entity &newEntity)
     registry.add_component<component::cposition_t>(registry.entity_from_index(enemy), std::move(position));
     component::cserverid_t serverId = {.id = newEntity.id};
     registry.add_component<component::cserverid_t>(registry.entity_from_index(enemy), std::move(serverId));
-    component::cvelocity_t velocity = {.velocity = 0};
+    component::cvelocity_t velocity = {.velocity = 4};
     registry.add_component<component::cvelocity_t>(registry.entity_from_index(enemy), std::move(velocity));
     registry.add_component<component::ckilled_t>(registry.entity_from_index(enemy), {false});
 
---
+++ Server/Source/Component/CTimer.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPP-500-PAR-5-1-rtype-martin.vanaud
+** File description:
+** CTimer
+*/
+
+#ifndef CTIMER_HPP_
+    #define CTIMER_HPP_
+
+    #include <chrono>
+
+    namespace component {
+        struct ctimer_t {
+            std::chrono::steady_clock::time_point deltaTime;
+        };
+    }
+
+#endif /* !CTIMER_HPP_ */
