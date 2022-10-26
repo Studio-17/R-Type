70319bbed3cd4ead4a141e56ba647c20f3722fe2
Maxime Carabina <maxime.carabina@epitech.eu>
2022-10-26 18:00:54 +0200

    fix(entity): fixed kill entity
    - in Entity.hpp, rename doxygen information
    - in Registry, refactor register_component function
    - in SparseArray.hpp, refactor erase function

--- Libraries/ECS/Source/Entity.hpp
+++ Libraries/ECS/Source/Entity.hpp
@@ -10,44 +10,36 @@
 
     #include <iostream>
 
-
 class Entity {
     public:
         /**
          * @brief Construct a new Entity object
-         * 
-         * @param id 
+         *
+         * @param id
          */
         explicit Entity(std::size_t id) : _id(id) {
         };
 
         /**
          * @brief Destroy the Entity object
-         * 
+         *
          */
         ~Entity() = default;
 
         /**
          * @brief  Operator () to return _id of the entity
-         * 
-         * @return std::size_t 
+         *
+         * @return std::size_t
          */
         operator std::size_t() const {
             return _id;
         };
 
     protected:
-        /**
-         * @brief  Friend object so that the Entoty can access the Registry object
-         * 
-         */
-        friend class Registry; 
+        friend class Registry; ///< Friend object so that the Entoty can access the Registry object
+
     private:
-        /**
-         * @brief  The Entity _id as a std::size_t
-         * 
-         */
-        std::size_t _id;
+        std::size_t _id; ///< The Entity _id as a std::size_t
 };
 
 #endif /* !ENTITY_HPP_ */
