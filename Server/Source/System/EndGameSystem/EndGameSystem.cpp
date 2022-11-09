278c916c79c059502a29bcaef0858e27e6cfec0b
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-11-09 13:43:40 +0100

    fix(forbidden id): lobbies id from FORBIDDEN_IDS NETWORK to LOBBY

--- Server/Source/System/EndGameSystem/EndGameSystem.cpp
+++ Server/Source/System/EndGameSystem/EndGameSystem.cpp
@@ -35,8 +35,7 @@ std::size_t countNbEntityInLobby(int lobbyId, ENTITY_TYPE entityType, Sparse_arr
 void System::EndGameSystem::operator()([[ maybe_unused ]] Registry &registry, [[ maybe_unused ]]Sparse_array<component::cnetwork_queue_t> &network_queues, [[ maybe_unused ]]Sparse_array<component::ctype_t> &type,[[ maybe_unused ]] Sparse_array<component::clobby_id_t> &lobbyIds, [[ maybe_unused ]]Sparse_array<component::clobbies_status_t> &lobbiesStatus)
 {
     for (int lobbyId = 1; lobbyId <= 3; lobbyId++) {
-    //     // std::cout << "EndGameSystem before" << std::endl;
-        if (!countNbEntityInLobby(lobbyId, ENTITY_TYPE::PLAYER, type, lobbyIds) && lobbiesStatus[FORBIDDEN_IDS::NETWORK].value().lobbiesStatus.at(lobbyId)) {
+        if (!countNbEntityInLobby(lobbyId, ENTITY_TYPE::PLAYER, type, lobbyIds) && lobbiesStatus[FORBIDDEN_IDS::LOBBY].value().lobbiesStatus.at(lobbyId).first) {
             std::cout << "EndGame System no spaceship alive"<<std::endl;
             for (std::size_t index = 0; index < lobbyIds.size() && index < type.size(); index++) {
                 if (!lobbyIds[index] || !type[index])
@@ -44,7 +43,7 @@ void System::EndGameSystem::operator()([[ maybe_unused ]] Registry &registry, [[
                 if (lobbyIds[index].value().id == lobbyId && std::find(_entityToKill.begin(), _entityToKill.end(), type[index].value().type) != _entityToKill.end())
                     registry.kill_entity(registry.entity_from_index(index));
             }
-            lobbiesStatus[FORBIDDEN_IDS::NETWORK].value().lobbiesStatus.at(lobbyId) = false;
+            lobbiesStatus[FORBIDDEN_IDS::LOBBY].value().lobbiesStatus.at(lobbyId).first = false;
             for (auto &entity : _entityToKill)
                 network_queues[FORBIDDEN_IDS::NETWORK].value().toSendNetworkQueue.push({lobbyId, serialize_header::serializeHeader<packet_kill_entity_type>(NETWORK_SERVER_TO_CLIENT::KILL_ENTITY_TYPE, {static_cast<int>(entity)})});
         }
