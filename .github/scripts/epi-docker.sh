e76a6070d6884039b8ce61c05bc8134138ca216a
Martin VANAUD <martin.vanaud@epitech.eu>
2022-11-13 13:45:47 +0100

    ci/cd(scripts) added various scripts for ci/cd manipulation

--- .github/scripts/epi-docker.sh
+++ .github/scripts/epi-docker.sh
@@ -6,4 +6,5 @@ SOURCE_FOLDER=$(pwd)
 # Specify destination folder to mount your project into docker
 DEST_FOLDER=/home/student
 
+# Run the container inside docker
 docker run --rm -v "$SOURCE_FOLDER:$DEST_FOLDER" -it epitechcontent/epitest-docker /bin/bash -c 'useradd student && su - student'
